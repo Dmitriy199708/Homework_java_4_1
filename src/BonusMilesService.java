e4859f593e8b787f0e12c76541712988e3554027
Dmitriy199708 <119492971+Dmitriy199708@users.noreply.github.com>
2023-10-03 20:53:20 +0300

    Editing Code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -2,8 +2,8 @@ public class BonusMilesService {
     public int calculate(int price) {
         int bonus;
         if (price >= 20) {
-           bonus = price / 20;
-        }else  {
+            bonus = price / 20;
+        } else {
             bonus = 0;
         }
         return bonus;
